296be50b6933b05f56f218b7e63cf2ec697ad8ba
Krab-Leitenant <timsottim@mail.ru>
2026-01-14 20:09:00 +0300

    Сделал часть кнопок лаунчеру но по причине отсутствия игры кнопка Начать игру спавнит камень, а кнопка ЗАКРЫТЬ убирает его

--- library/base.py
+++ library/base.py
@@ -1,38 +1,81 @@
 import arcade
+from arcade.gui import UIManager, UITextureButton, UITextArea, UILabel
+from arcade.gui.widgets.layout import UIAnchorLayout, UIBoxLayout
+
 from time import sleep
 
 class Launcher(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title, resizable=True)
         self.sprite_list = arcade.SpriteList()
+        self.manager = UIManager()
+        self.manager.enable()
+        self.anchor_layout_left = UIAnchorLayout(x=-450, y=200)
+        self.anchor_layout_main = UIAnchorLayout(y=-200)
+        self.box_layout_main = UIBoxLayout(vertical=False, space_between=50)
+        self.box_layout_left = UIBoxLayout(vertical=True, space_between=20)
+        self.setup_widgets()
+
+        self.anchor_layout_left.add(self.box_layout_left)
+        self.anchor_layout_main.add(self.box_layout_main)
+        self.manager.add(self.anchor_layout_main)
+        self.manager.add(self.anchor_layout_left)
         self.width, self.height = width, height
+
+        self.button_normal_texture = arcade.load_texture('../images/button.png')
+        self.button_pressed_texture = arcade.load_texture('../images/button_pressed.png')
     def setup(self):
         self.background_color = arcade.color.TEA_GREEN
         self.texture = arcade.load_texture('../images/L_background.jpg')
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
+    def clear_sprites(self):
+        for sprite in self.sprite_list:
+            sprite.remove_from_sprite_lists()
+
+
+    def setup_widgets(self):
+        wortroll_label = UITextArea(text='Wortroll \n games', x=900, y=650, font_size=50, font_name='arial', text_color=arcade.color.RED, width=300, height=200)
+        button_normal_texture = arcade.load_texture('../images/button.png')
+        button_pressed_texture = arcade.load_texture('../images/button_pressed.png')
+        button_main1 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture, width=400, height=80, text='Начать игру')
+        button_main2 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture, width=200,
+                                       height=80, text='ЗАКРЫТЬ')
+        button = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture, width=300, height=80, text='ДОНАТ 10 РУБЛЕЙ')
+        button1 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture,
+                                 width=300, height=80, text='ДОНАТ 100 РУБЛЕЙ')
+        button2 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture,
+                                 width=300, height=80, text='ДОНАТ 1000 РУБЛЕЙ')
+        button3 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture,
+                                 width=300, height=80, text='ДОНАТ 10000 РУБЛЕЙ')
+        button_main1.on_click = lambda event:self.add_sprite(arcade.Sprite(path_or_texture='../images/block.png', center_x=600, center_y=500, scale=10)) if len(self.sprite_list) ==0 else None
+        button_main2.on_click = lambda event: self.clear_sprites()
+        button1.on_click = lambda event: print("Вы задонатили 1рублей!!!")
+        button2.on_click = lambda event: print("Вы задонатили 100 рублей!!!")
+        button3.on_click = lambda event: print("Вы задонатили 1000 рублей!!!")
+        button.on_click = lambda event: print("Вы задонатили 10000 рублей!!!")
+        self.box_layout_main.add(button_main1)
+        self.box_layout_main.add(button_main2)
+        self.manager.add(wortroll_label)
+        self.box_layout_left.add(button)
+        self.box_layout_left.add(button1)
+        self.box_layout_left.add(button2)
+        self.box_layout_left.add(button3)
 
     def on_draw(self):
         self.clear()
+
         arcade.draw_texture_rect(self.texture, arcade.rect.XYWH(self.width // 2, self.height // 2, self.width, self.height))
         self.sprite_list.draw()
-
-        for i in self.sprite_list:
-            try:
-                i.draw_text()
-            except:
-                pass
+        self.manager.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        pushed_button = arcade.get_sprites_at_point((x, y), self.sprite_list)
-        for button in pushed_button:
-
-                button.on_press()
+        pass
 
 
 
 
-class Button(arcade.Sprite):
+"""class Button(arcade.Sprite):
     def __init__(self, center_x, center_y, scale=1, text='test', command=None, **kwargs):
         super().__init__(path_or_texture='../images/button.png', scale=scale, center_x=center_x, center_y=center_y,
                          **kwargs)
@@ -49,12 +92,11 @@ class Button(arcade.Sprite):
 
 def command():
     print('test')
+"""
 
 
 def main():
     mw = Launcher(1255, 857, "Лаунчер")
-    b1 = Button(center_x=1255//2, center_y=300, scale=2, text='Начать игру', command=command())
-    mw.add_sprite(b1)
 
     mw.setup()
     arcade.run()
