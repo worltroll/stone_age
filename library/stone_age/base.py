a833d576e470d6c9af6c012e838b82b197ccfeea
Worltroll <SVINOPTAM@yandex.ru>
2026-01-14 19:44:54 +0300

    окончил визуальную часть хотбара и инвентаря

--- library/stone_age/base.py
+++ library/stone_age/base.py
@@ -1,5 +1,5 @@
 from environmental import Grass
-from GUI import HotBar
+from GUI import HotBar, Inventory
 from library.random_tools import random_coords
 
 import arcade
@@ -13,14 +13,61 @@ class Game(arcade.Window):
     def setup(self):
         self.generate_map()
         self.hotbar = HotBar()
+        self.inventory = Inventory()
         self.background_color = arcade.color.TEA_GREEN
+
+        self.speed = 3
+
+        self.inventory_flag = False
+        self.up_flag = False
+        self.left_flag = False
+        self.down_flag = False
+        self.right_flag = False
 
     def on_draw(self):
         self.clear()
 
+        arcade.set_background_color(self.background_color)
+
         self.update_map()
         self.hotbar.update()
         self.hotbar.draw()
+
+        if self.inventory_flag:
+            self.inventory.draw()
+
+    def on_key_press(self, symbol, modifiers):
+        match symbol:
+            case arcade.key.KEY_1:
+                self.hotbar.select(0)
+            case arcade.key.KEY_2:
+                self.hotbar.select(1)
+            case arcade.key.KEY_3:
+                self.hotbar.select(2)
+            case arcade.key.KEY_4:
+                self.hotbar.select(3)
+            case arcade.key.KEY_5:
+                self.hotbar.select(4)
+            case arcade.key.KEY_6:
+                self.hotbar.select(5)
+            case arcade.key.KEY_7:
+                self.hotbar.select(6)
+            case arcade.key.KEY_8:
+                self.hotbar.select(7)
+            case arcade.key.KEY_9:
+                self.hotbar.select(8)
+            case arcade.key.KEY_0:
+                self.hotbar.select(9)
+            case arcade.key.E:
+                self.inventory_flag = not self.inventory_flag
+            case arcade.key.W:
+                self.up_flag = not self.up_flag
+            case arcade.key.A:
+                self.left_flag = not self.left_flag
+            case arcade.key.S:
+                self.down_flag = not self.down_flag
+            case arcade.key.D:
+                self.right_flag = not self.right_flag
 
     def update_map(self):
         for i in self.environment:
@@ -38,6 +85,6 @@ class Game(arcade.Window):
 
 
 if __name__ == "__main__":
-    mw = Game(1346, 800, "Каменный век")
+    mw = Game(1346, 834, "Каменный век")
     mw.setup()
     arcade.run()
