add91d1e64dc039eec5390b5667590b0a7c18b68
Krab-Leitenant <timsottim@mail.ru>
2026-01-16 19:29:54 +0300

    Сделал часть кнопок лаунчеру но по причине отсутствия игры кнопка Начать игру спавнит камень, а кнопка ЗАКРЫТЬ убирает его

--- library/base.py
+++ library/base.py
@@ -1,5 +1,5 @@
 import arcade
-from arcade.gui import UIManager, UITextureButton, UITextArea, UILabel
+from arcade.gui import UIManager, UITextureButton, UITextArea, UILabel, UIStyleBase
 from arcade.gui.widgets.layout import UIAnchorLayout, UIBoxLayout
 
 from time import sleep
@@ -22,11 +22,14 @@ class Launcher(arcade.Window):
         self.manager.add(self.anchor_layout_left)
         self.width, self.height = width, height
 
-        self.button_normal_texture = arcade.load_texture('../images/button.png')
-        self.button_pressed_texture = arcade.load_texture('../images/button_pressed.png')
+
+
     def setup(self):
         self.background_color = arcade.color.TEA_GREEN
         self.texture = arcade.load_texture('../images/L_background.jpg')
+
+
+
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
     def clear_sprites(self):
@@ -35,18 +38,20 @@ class Launcher(arcade.Window):
 
 
     def setup_widgets(self):
-        wortroll_label = UITextArea(text='Wortroll \n games', x=900, y=650, font_size=50, font_name='arial', text_color=arcade.color.RED, width=300, height=200)
+
+        wortroll_label = UITextArea(text='Wortroll \n games', x=900, y=650, font_size=50, text_color=arcade.color.RED, width=300, height=200)
         button_normal_texture = arcade.load_texture('../images/button.png')
         button_pressed_texture = arcade.load_texture('../images/button_pressed.png')
-        button_main1 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture, width=400, height=80, text='Начать игру')
-        button_main2 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture, width=200,
+        button_hovered_texture = arcade.load_texture('../images/button_hovered.png')
+        button_main1 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture,texture_hovered=button_hovered_texture, width=400, height=80, text='Начать игру')
+        button_main2 = UITextureButton(texture=button_normal_texture, texture_hovered=button_hovered_texture, texture_pressed=button_pressed_texture, width=200,
                                        height=80, text='ЗАКРЫТЬ')
-        button = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture, width=300, height=80, text='ДОНАТ 10 РУБЛЕЙ')
-        button1 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture,
+        button = UITextureButton(texture=button_normal_texture, texture_hovered=button_hovered_texture, texture_pressed=button_pressed_texture, width=300, height=80, text='ДОНАТ 10 РУБЛЕЙ')
+        button1 = UITextureButton(texture=button_normal_texture, texture_hovered=button_hovered_texture, texture_pressed=button_pressed_texture,
                                  width=300, height=80, text='ДОНАТ 100 РУБЛЕЙ')
-        button2 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture,
+        button2 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture,texture_hovered=button_hovered_texture,
                                  width=300, height=80, text='ДОНАТ 1000 РУБЛЕЙ')
-        button3 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture,
+        button3 = UITextureButton(texture=button_normal_texture, texture_pressed=button_pressed_texture,texture_hovered=button_hovered_texture,
                                  width=300, height=80, text='ДОНАТ 10000 РУБЛЕЙ')
         button_main1.on_click = lambda event:self.add_sprite(arcade.Sprite(path_or_texture='../images/block.png', center_x=600, center_y=500, scale=10)) if len(self.sprite_list) ==0 else None
         button_main2.on_click = lambda event: self.clear_sprites()
