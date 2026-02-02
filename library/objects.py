6ce3efbe6f55e5fa09f975c498ec23b5b5c61605
Krab-Leitenant <timsottim@mail.ru>
2026-02-02 19:22:08 +0300

    сделал еще часть настроек и попытался сделать совй класс тамблера для них но он почему то очень криво работает, может ты посмотришь и придумаешь как это пофиксить(в настройках графики я тамблер разместил пока)

--- library/objects.py
+++ library/objects.py
@@ -1,5 +1,5 @@
 import arcade
-from library.base import Launcher, Button
+from library.base import Launcher
 mw = Launcher(1255, 857, "Лаунчер")
 b1 = Button(center_x=1255//2, center_y=300, scale=1, text='Начать игру')
 mw.add_sprite(b1)
